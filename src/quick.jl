974772a952adeb4e9e1236ade993a67286964dd2
Isaiah <isaiah.norton@gmail.com>
2013-04-13 22:58:38 -0400

    update quick.jl debugging helper

--- src/quick.jl
+++ src/quick.jl
@@ -1,3 +1,7 @@
+const extra_inc_dirs =["Common", "Utilities", "VolumeRendering", "Rendering"]
+const extra_inc_paths=map(x -> joinpath("-I/cmn/git/VTK/", x), extra_inc_dirs)
+insert!(extra_inc_paths, 1, "-I/cmn/git/VTK5101-build/")
+
 using Clang.cindex, Clang.wrap_c, Clang.wrap_cpp
 
   idx = cindex.idx_create(0,1)
@@ -5,6 +9,7 @@ using Clang.cindex, Clang.wrap_c, Clang.wrap_cpp
     ["-x", "c++",
      "-I/cmn/git/VTK5101-build/includes",
      "-I/cmn/git/julia/deps/llvm-3.2/build/Release/lib/clang/3.2/include",
+     extra_inc_paths...,
      "-v", "-c"])
 
   topcu = cindex.getTranslationUnitCursor(tu)
